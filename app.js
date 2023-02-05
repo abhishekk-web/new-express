532182044606879a5d41c609627caa4d1b5aa72c
abhishekk-web <abhi.chatterjee38@gmail.com>
2023-02-05 10:07:49 +0530

    express and bodyparser are now working in node js

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const http = require('http');
-
 const express = require("express");
-
+const bodyParser = require('body-parser');
 
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) =>{
+// app.use((req, res, next) =>{
 
-    console.log("In the middleware");
-    next();
+//     console.log("In the middleware");
+//     next();
 
+// })
+
+app.get("/add-product",(req, res, next) => {
+    console.log("In another middleware");
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type='submit'>Add Product</form>")
 })
 
-app.use((req, res, next) => {
-    console.log("In another middleware");
-    res.send("<h1>Hello world</h1>")
+app.post('/product', (req, res, next)=> {
+    console.log(req.body);
+    res.redirect("/about");
+})
+
+app.use("/about", (req, res, next) => {
+    console.log("In the second middleware");
+    res.send("<h1>Hello world again</h1>")
 })
 
 
